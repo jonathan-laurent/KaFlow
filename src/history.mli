7fdb47a3c83f9fd5d4895c89856372e0fb2441d5
Jonathan Laurent <jonathan.laurent@cs.cmu.edu>
2017-07-04 15:01:07 -0400

    Array-based implementation of History. Code does NOT compile.

--- src/history.mli
+++ src/history.mli
@@ -1,27 +1,10 @@
-(* Sparse persistent dictionnaries indexed by integers.
-   This structure is optimized to answer requests of the kind
+(* Sets of integers that are optimized to answer requests of the form:
    "what is the last element in t whose index is strictly greater than i" *)
 
-module type S = sig
+  type t
 
-type 'a t
+  val from_queue : int Queue.t -> t
 
-val init : int -> 'a t
-
-val empty : 'a t
-
-val size : 'a t -> int
-
-val range : 'a t -> int
-
-val add : int -> 'a -> 'a t -> 'a t
-
-val last_before : int -> 'a t -> (int * 'a) option
-
-val first_after : int -> 'a t -> (int * 'a) option
-
-val add_list : (int * 'a) list -> 'a t -> 'a t
-
-end
-
-include S
+  val first_after : int -> t -> int option
+  
+  val last_before : int -> t -> int option
